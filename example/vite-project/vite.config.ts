7649a773c43ce1d2a664626bf625ba8389a50328
cuixiaorui <cui_xiaorui@126.com>
2022-04-29 21:35:08 +0800

    refactor: use compiler-sfc parse replace regular

--- example/vite-project/vite.config.ts
+++ example/vite-project/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { compile } from "../../lib/vue3-template-magic.esm.js";
+import { parse } from "@vue/compiler-sfc";
 
 // self plugin
 
@@ -9,22 +10,21 @@ function templateTransformPlugin() {
     name: "templateTransformPlugin",
     transform(code: string, id: string) {
       if (id.endsWith(".vue")) {
-        const templateCode = /<template>([\s\S]+)<\/template>/g.exec(code);
-     
+        const { descriptor: {template} } = parse(code);
+
         const plugin = (code) => {
           if (code.type === 1) {
             code.tag = "span";
           }
         };
-        
-        const { code: transformedTemplateCode } = compile(templateCode[1], {
+
+        const { code: transformedTemplateCode } = compile(template.content, {
           nodeTransforms: [plugin],
         });
         code = code.replace(
           /<template>([\s\S]+)<\/template>/g,
           `<template>${transformedTemplateCode}</template>`
         );
-
       }
 
       return code;
