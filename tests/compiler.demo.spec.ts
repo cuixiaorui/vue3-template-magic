f655d79ac0f6f2e28290e2719c0c621235f27a38
cuixiaorui <cui_xiaorui@126.com>
2022-05-02 16:26:33 +0800

    chore: remove demo test

--- tests/compiler.demo.spec.ts
+++
@@ -1,6 +0,0 @@
-import { NodeTypes } from "@vue/compiler-core";
-import { test } from "vitest";
-test("test demo", () => {
-  // TypeError: Cannot read properties of undefined (reading 'ELEMENT')
-  console.log(NodeTypes.ELEMENT);
-});
